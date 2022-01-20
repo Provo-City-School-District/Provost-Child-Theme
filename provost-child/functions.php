e6ca6e2f2216def5599c94590565b5ba69f13513
Espi <joshespi@gmail.com>
2022-01-20 14:36:27 -0700

    initial upload

--- provost-child/functions.php
+++ provost-child/functions.php
@@ -1,3 +1,14 @@
+<?php
+/*==========================================================================================
+Add stylesheets/javascripts to enqueue
+============================================================================================*/
+function my_theme_enqueue_styles() {
+
+    wp_enqueue_style( 'parent-theme', get_template_directory_uri() .'/style.css', '', '1.0.1', false);
+    wp_enqueue_script( 'child_scripts', get_theme_file_uri().'/assets/js/child-scripts.js', '', '1.0.13', true);
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', '' , '1.0.1', false);
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 /*==========================================================================================
 // Favicon
 ============================================================================================*/
@@ -10,3 +21,39 @@ function pcsd_add_favicon(){ ?>
 add_action('wp_head','pcsd_add_favicon');
 //add the favicon to the login page
 add_action('login_head','pcsd_add_favicon');
+/*==========================================================================================
+Custom Excerpt
+============================================================================================*/
+function get_excerpt(){
+	$excerpt = get_the_content();
+	//$excerpt = preg_replace(" ([.*?])",'',$excerpt);
+	$excerpt = strip_shortcodes($excerpt);
+	$excerpt = strip_tags($excerpt);
+	$excerpt = substr($excerpt, 0, 200);
+	$excerpt = substr($excerpt, 0, strripos($excerpt, " "));
+	$excerpt = trim(preg_replace( '/\s+/', ' ', $excerpt));
+	$excerpt = '<p>'.$excerpt.'...'.'</p>';
+
+	return $excerpt;
+}
+/*==========================================================================================
+// custom Login Page
+============================================================================================*/
+function my_custom_login() {
+echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/login/custom-login-styles.css" />';
+}
+add_action('login_head', 'my_custom_login');
+
+function my_login_logo_url() {
+return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+return 'Elem School | Provo City School District';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+/*==========================================================================================
+// ShortCodes
+============================================================================================*/
---
+++ provost-child/template-parts/mega-menu-dropdowns-titlei.php
@@ -0,0 +1,78 @@
+				<div class="dropDownColumn">
+					<?php
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h2>
+							<?php
+						} else {
+							?>
+							<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h3>
+							<?php
+						}
+					?>
+						<ul>
+							<li class="int"><a href="https://provost.provo.edu/title-i/what-is-title-i/">What is Title I?</a></li>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2019/10/Provost-FY20-SIP-4.pdf">School Improvement Plan</a></li>
+						</ul>
+						<p>Title I Coordinator: Jeannette Moreira (jeannettem@provo.edu)</p>
+
+				</div>
+				<div class="dropDownColumn">
+					<?php
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />School Parent Family Compact</h2>
+							<?php
+						} else {
+							?>
+							<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />School Parent Family Compact</h3>
+							<?php
+						}
+					?>
+
+						<ul>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2021/10/provost_ParentCompact_fy21_Eng.pdf">School Parent Family Compact (English)</a></li>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2021/10/PV_ParentCompact_fy22_Spanish.pdf">School Parent Family Compact (Spanish)</a></li>
+						</ul>
+					<?php
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent &amp; Family Engagement Policy</h2>
+							<?php
+						} else {
+							?>
+							<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent &amp; Family Engagement Policy</h3>
+							<?php
+						}
+					?>
+
+						<ul>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2021/10/provost_ParentEngagementPolicy_fy21_Eng.pdf">Parent & Family Engagement Policy (English)</a></li>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2021/10/PV_ParentEngagementPolicy_fy22_Spanish.pdf">Parent & Family Engagement Policy (Spanish)</a></li>
+						</ul>
+
+				</div>
+				<div class="dropDownColumn">
+					<?php
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h2>
+							<?php
+						} else {
+							?>
+							<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h3>
+							<?php
+						}
+					?>
+
+						<ul>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/Title-I-Parent-Right-to-Know-EngSpan.pdf">Title 1 Patrons right to know</a></li>
+							<li class="ext"><a href="https://provo.edu/title-i/">District Title I</a></li>
+							<li class="int"><a href="https://utahschoolgrades.schools.utah.gov/home/SchoolAchievement/?StateID=99&SchoolID=1231&DistrictID=1222&SchoolNbr=120&SchoolLevel=K8&IsSplitSchool=0&schoolyearendyear=2021">School Report Card 2020-2021</a></li>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2020/03/PV_Goals_1page_fy20.pdf"><span class="hidden">Provost Elementary </span>Goals At A Glance Fy20</a></li>
+						</ul>
+				</div>
